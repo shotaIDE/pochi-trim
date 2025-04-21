76e43e4c3ea5c2d142c8ac7723eea68832aa3ff5
Shota Ide <ide.shota.dev@gmail.com>
2025-04-21 22:05:15 +0900

    Update house_id_provider.dart

--- client/lib/services/house_id_provider.dart
+++ client/lib/services/house_id_provider.dart
@@ -1,9 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:house_worker/services/auth_service.dart';
 
-/// 現在のハウスIDを提供するプロバイダー
-/// アプリケーション全体で一貫したハウスIDを使用するために使用します
+// ハウスIDを提供するプロバイダー
 final currentHouseIdProvider = Provider<String>((ref) {
-  final authService = ref.watch(authServiceProvider);
-  return authService.currentUser?.uid ?? '';
+  // TODO(ide): 適切な値に置き換える
+  return 'default-house-id';
 });
