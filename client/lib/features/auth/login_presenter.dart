02417e6ec80a9a96cfd2b6737c874937cecb1729
shotaIDE <ide.shota.dev@gmail.com>
2025-04-30 23:51:40 +0900

    Update login_presenter.dart

--- client/lib/features/auth/login_presenter.dart
+++ client/lib/features/auth/login_presenter.dart
@@ -1,23 +1,16 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:house_worker/models/sign_in_result.dart';
 import 'package:house_worker/services/auth_service.dart';
 import 'package:house_worker/services/functions_service.dart';
 import 'package:house_worker/services/house_id_provider.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-part 'login_presenter.freezed.dart';
 part 'login_presenter.g.dart';
-
-@freezed
-abstract class LoginResultValue with _$LoginResultValue {
-  const factory LoginResultValue({required bool isSuccess}) = _LoginResultValue;
-}
 
 @riverpod
 class LoginButtonTappedResult extends _$LoginButtonTappedResult {
   @override
-  Future<LoginResultValue?> build() async {
-    return null;
+  Future<void> build() async {
+    return;
   }
 
   Future<void> onLoginTapped() async {
