e17caa39d148e7b2d04e86ac751dfc93e3a4894c
Shota Ide <ide.shota.dev@gmail.com>
2025-05-04 21:24:49 +0900

    Update max_house_work_limit_exceeded_exception.dart

--- client/lib/exceptions/max_house_work_limit_exceeded_exception.dart
+++ client/lib/exceptions/max_house_work_limit_exceeded_exception.dart
@@ -1,6 +1,1 @@
-class MaxHouseWorkLimitExceededException implements Exception {
-  final message = 'フリー版では最大10件までの家事しか登録できません。Pro版にアップグレードすると、無制限に家事を登録できます。';
-
-  @override
-  String toString() => message;
-}
+class MaxHouseWorkLimitExceededException implements Exception {}
