fbed703e777722a618ee7a41fdd988242c255885
Shota Ide <ide.shota.dev@gmail.com>
2025-05-06 09:57:20 +0900

    Update house_work_item.dart

--- client/lib/features/home/house_work_item.dart
+++ client/lib/features/home/house_work_item.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:house_worker/models/house_work.dart';
 
-class HouseWorkItem extends StatelessWidget {
+class HouseWorkItem extends StatefulWidget {
   const HouseWorkItem({
     super.key,
     required this.houseWork,
@@ -16,15 +16,36 @@ class HouseWorkItem extends StatelessWidget {
   final void Function(HouseWork) onDelete;
 
   @override
+  State<HouseWorkItem> createState() => _HouseWorkItemState();
+}
+
+class _HouseWorkItemState extends State<HouseWorkItem> {
+  var _isCompleting = false;
+
+  @override
   Widget build(BuildContext context) {
     final deleteIcon = Icon(
       Icons.delete,
       color: Theme.of(context).colorScheme.onError,
     );
 
-    final doCompleteIcon = Icon(
-      Icons.check_circle_outline,
-      color: Theme.of(context).colorScheme.onSurface,
+    final doCompleteIcon = AnimatedSwitcher(
+      duration: const Duration(milliseconds: 150),
+      transitionBuilder: (Widget child, Animation<double> animation) {
+        return FadeTransition(opacity: animation, child: child);
+      },
+      child:
+          _isCompleting
+              ? Icon(
+                Icons.check_circle,
+                key: const ValueKey('check_circle'),
+                color: Theme.of(context).colorScheme.primary,
+              )
+              : Icon(
+                Icons.check_circle_outline,
+                key: const ValueKey('check_circle_outline'),
+                color: Theme.of(context).colorScheme.onSurface,
+              ),
     );
     final houseWorkIcon = Container(
       alignment: Alignment.center,
@@ -35,16 +56,16 @@ class HouseWorkItem extends StatelessWidget {
       width: 40,
       height: 40,
       child: Text(
-        houseWork.icon,
+        widget.houseWork.icon,
         style: Theme.of(context).textTheme.headlineSmall,
       ),
     );
     final houseWorkTitleText = Text(
-      houseWork.title,
+      widget.houseWork.title,
       style: Theme.of(context).textTheme.titleMedium,
     );
     final completeButtonPart = InkWell(
-      onTap: () => onCompleteTap(houseWork),
+      onTap: _onTap,
       child: Padding(
         padding: const EdgeInsets.all(16),
         child: Row(
@@ -72,7 +93,7 @@ class HouseWorkItem extends StatelessWidget {
 
     const dashboardIcon = Icon(Icons.chevron_right);
     final moveToDashboardPart = InkWell(
-      onTap: () => onMoveTap(houseWork),
+      onTap: () => widget.onMoveTap(widget.houseWork),
       child: const Column(
         children: [
           Expanded(
@@ -100,7 +121,7 @@ class HouseWorkItem extends StatelessWidget {
     );
 
     return Dismissible(
-      key: Key('houseWork-${houseWork.id}'),
+      key: Key('houseWork-${widget.houseWork.id}'),
       background: Container(
         color: Theme.of(context).colorScheme.error,
         alignment: Alignment.centerRight,
@@ -109,11 +130,27 @@ class HouseWorkItem extends StatelessWidget {
       ),
       direction: DismissDirection.endToStart,
       confirmDismiss: (direction) async {
-        onDelete(houseWork);
+        widget.onDelete(widget.houseWork);
 
         return false;
       },
       child: item,
     );
+  }
+
+  void _onTap() {
+    setState(() {
+      _isCompleting = true;
+    });
+
+    Future.delayed(const Duration(milliseconds: 300), () {
+      if (mounted) {
+        setState(() {
+          _isCompleting = false;
+        });
+      }
+    });
+
+    widget.onCompleteTap(widget.houseWork);
   }
 }
