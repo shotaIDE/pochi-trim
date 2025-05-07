a711f78d923e73c021ed198a253067fa745ce293
Shota Ide <ide.shota.dev@gmail.com>
2025-05-07 15:04:27 +0900

    Update bar_chart_touched_position.dart

--- client/lib/features/analysis/bar_chart_touched_position.dart
+++ client/lib/features/analysis/bar_chart_touched_position.dart
@@ -5,8 +5,13 @@ part 'bar_chart_touched_position.freezed.dart';
 @freezed
 abstract class BarChartTouchedPosition with _$BarChartTouchedPosition {
   const factory BarChartTouchedPosition({
+    /// X軸の値
     required int groupIndex,
+
+    /// X軸の値におけるサブの値
     required int rodDataIndex,
+
+    /// 積み上げ棒グラフにおける位置番号
     required int stackItemIndex,
   }) = _BarChartTouchedPosition;
 }
