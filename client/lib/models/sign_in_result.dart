4cfc067c86be8f1471c2fa69411f3e6975b0739b
shotaIDE <ide.shota.dev@gmail.com>
2025-05-08 15:04:32 +0900

    Update sign_in_result.dart

--- client/lib/models/sign_in_result.dart
+++ client/lib/models/sign_in_result.dart
@@ -1,1 +1,22 @@
-class SignInException implements Exception {}
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'sign_in_result.freezed.dart';
+
+@freezed
+sealed class SignInResult with _$SignInResult {
+  const factory SignInResult.success({
+    required String userId,
+    required bool isNewUser,
+  }) = SignInSuccess;
+
+  const factory SignInResult.cancelled() = SignInCancelled;
+}
+
+@freezed
+sealed class SignInException with _$SignInException implements Exception {
+  const factory SignInException.general() = GeneralSignInException;
+
+  const factory SignInException.google() = GoogleSignInException;
+
+  const factory SignInException.accountLink() = AccountLinkException;
+}
