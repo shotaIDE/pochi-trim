5fcab54319cfe19654833552d3c039de5b933ff1
Shota Ide <ide.shota.dev@gmail.com>
2025-05-09 21:42:57 +0900

    Update root_presenter.dart

--- client/lib/root_presenter.dart
+++ client/lib/root_presenter.dart
@@ -3,6 +3,7 @@ import 'package:house_worker/app_initial_route.dart';
 import 'package:house_worker/models/app_session.dart';
 import 'package:house_worker/models/preference_key.dart';
 import 'package:house_worker/models/root_app_not_initialized.dart';
+import 'package:house_worker/services/app_info_service.dart';
 import 'package:house_worker/services/auth_service.dart';
 import 'package:house_worker/services/preference_service.dart';
 import 'package:house_worker/services/remote_config_service.dart';
@@ -18,7 +19,11 @@ Future<AppInitialRoute> appInitialRoute(Ref ref) async {
       .ensureActivateFetchedRemoteConfigs();
 
   final minimumBuildNumber = ref.watch(minimumBuildNumberProvider);
-  // TODO(ide): 強制アップデートの実装
+  final currentAppVersion = await ref.watch(currentAppVersionProvider.future);
+  final currentBuildNumber = currentAppVersion.buildNumber;
+  if (currentBuildNumber < minimumBuildNumber) {
+    return AppInitialRoute.updateApp;
+  }
 
   final appSession = ref.watch(currentAppSessionProvider.future);
   switch (appSession) {
