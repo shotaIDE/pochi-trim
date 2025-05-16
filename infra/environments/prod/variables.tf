44ad965c61a40c8fa00b2f6eae3ec53ff5d7b3ff
Shota Ide <ide.shota.dev@gmail.com>
2025-05-16 09:48:38 +0900

    refactor

--- infra/environments/prod/variables.tf
+++ infra/environments/prod/variables.tf
@@ -1,16 +1,16 @@
 variable "google_billing_account_id" {
   type        = string
-  description = "請求先アカウントID"
+  description = "Google Cloudの請求先アカウントID"
 }
 
 variable "google_project_location" {
   type        = string
-  description = "GCPプロジェクトのロケーション"
+  description = "Google Cloudプロジェクトのロケーション"
 }
 
 variable "ios_app_team_id" {
   type        = string
-  description = "iOS開発者チームID"
+  description = "Apple開発者アカウントのチームID"
 }
 
 variable "firebase_android_app_sha1_hashes" {
