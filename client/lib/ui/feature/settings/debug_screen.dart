814665e31f9bf0a5a2d8d1ce067817e2739073ed
Shota Ide <ide.shota.dev@gmail.com>
2025-05-21 17:11:15 +0900

    Update debug_screen.dart

--- client/lib/ui/feature/settings/debug_screen.dart
+++ client/lib/ui/feature/settings/debug_screen.dart
@@ -1,7 +1,9 @@
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pochi_trim/data/service/in_app_purchase_service.dart';
 import 'package:pochi_trim/ui/feature/settings/section_header.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 
 class DebugScreen extends ConsumerWidget {
   const DebugScreen({super.key});
@@ -20,6 +22,8 @@ class DebugScreen extends ConsumerWidget {
       appBar: AppBar(title: const Text('デバッグ')),
       body: ListView(
         children: const [
+          SectionHeader(title: 'RevenueCat'),
+          _ToggleIsProTile(),
           SectionHeader(title: 'Crashlytics'),
           _ForceErrorTile(),
           _ForceCrashTile(),
@@ -47,5 +51,42 @@ class _ForceErrorTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListTile(title: const Text('強制エラー'), onTap: () => throw Exception());
+  }
+}
+
+class _ToggleIsProTile extends ConsumerWidget {
+  const _ToggleIsProTile();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isProFuture = ref.watch(isProUserProvider.future);
+
+    return FutureBuilder(
+      future: isProFuture,
+      builder: (context, snapshot) {
+        final isPro = snapshot.data;
+        final String isProText;
+        if (isPro == null) {
+          isProText = '不明';
+        } else {
+          isProText = isPro ? 'Pro版' : 'フリー版';
+        }
+
+        return ListTile(
+          title: const Text('Pro版有効状態をトグル'),
+          trailing: Skeletonizer(
+            enabled: isPro == null,
+            child: Text(isProText),
+          ),
+          onTap: () {
+            if (isPro == null) {
+              return;
+            }
+
+            ref.read(isProUserProvider.notifier).setProUser(isPro: !isPro);
+          },
+        );
+      },
+    );
   }
 }
