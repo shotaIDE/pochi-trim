700c902a8868a94d8b7629f6d5aa8c1f949da029
Shota Ide <ide.shota.dev@gmail.com>
2025-05-21 22:46:12 +0900

    Update flavor.dart

--- client/lib/data/definition/flavor.dart
+++ client/lib/data/definition/flavor.dart
@@ -4,15 +4,15 @@ enum Flavor { emulator, dev, prod }
 
 final Flavor flavor = _getFlavor();
 
-class NoFlavorDetectedError extends Error {}
-
 Flavor _getFlavor() {
   final logger = Logger('Flavor');
 
   const flavorString = String.fromEnvironment('FLUTTER_APP_FLAVOR');
   final flavor = Flavor.values.firstWhere(
     (value) => value.name == flavorString,
-    orElse: () => throw NoFlavorDetectedError(),
+    orElse:
+        () =>
+            throw UnimplementedError('Flavor $flavorString is not implemented'),
   );
 
   logger.info('Detected flavor: ${flavor.name}');
