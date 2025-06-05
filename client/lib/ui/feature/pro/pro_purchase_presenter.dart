3a8f8d0dc5a4f5cd7a34a27dc6faa7c299e67329
Shota Ide <shotaIDE@users.noreply.github.com>
2025-06-05 21:18:19 +0900

    feat: restore purchases (#183)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- client/lib/ui/feature/pro/pro_purchase_presenter.dart
+++ client/lib/ui/feature/pro/pro_purchase_presenter.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:pochi_trim/data/definition/app_definition.dart';
 import 'package:pochi_trim/data/model/purchasable.dart';
+import 'package:pochi_trim/data/service/in_app_purchase_service.dart';
 import 'package:pochi_trim/ui/feature/pro/purchase_exception.dart';
 import 'package:pochi_trim/ui/root_presenter.dart';
 import 'package:purchases_flutter/purchases_flutter.dart';
@@ -30,9 +29,35 @@ Future<void> purchaseResult(Ref ref, Purchasable product) async {
     }
   }
 
-  if (customerInfo.entitlements.active[revenueCatProEntitlementId] == null) {
+  if (!hasProEntitlement(customerInfo: customerInfo)) {
     throw const PurchaseException.uncategorized();
   }
 
   await ref.read(currentAppSessionProvider.notifier).upgradeToPro();
+}
+
+/// 購入履歴を復元する
+///
+/// 復元に失敗した場合は、[RestorePurchaseException]を投げる。
+@riverpod
+Future<void> restorePurchaseResult(Ref ref) async {
+  final CustomerInfo customerInfo;
+  try {
+    customerInfo = await Purchases.restorePurchases();
+  } on PlatformException catch (e) {
+    final errorCode = PurchasesErrorHelper.getErrorCode(e);
+
+    switch (errorCode) {
+      case PurchasesErrorCode.invalidReceiptError:
+        throw const RestorePurchaseException.notFound();
+      // ignore: no_default_cases
+      default:
+        throw const PurchaseException.uncategorized();
+    }
+  }
+
+  // Pro版のエンタイトルメントがアクティブかチェック
+  if (hasProEntitlement(customerInfo: customerInfo)) {
+    await ref.read(currentAppSessionProvider.notifier).upgradeToPro();
+  }
 }
