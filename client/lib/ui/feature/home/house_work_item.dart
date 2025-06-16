fdc0ec37d76bded61cdd2f36bd1719cf5e5af0f3
Shota Ide <ide.shota.dev@gmail.com>
2025-06-16 21:58:56 +0900

    Update house_work_item.dart

--- client/lib/ui/feature/home/house_work_item.dart
+++ client/lib/ui/feature/home/house_work_item.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:pochi_trim/data/model/house_work.dart';
 
+enum _ModalAction { delete }
+
 class HouseWorkItem extends StatefulWidget {
   const HouseWorkItem({
     super.key,
@@ -58,7 +60,7 @@ class _HouseWorkItemState extends State<HouseWorkItem> {
     );
     final item = GestureDetector(
       onTap: _onTap,
-      onLongPress: () => widget.onDelete(widget.houseWork),
+      onLongPress: _onLongPress,
       child: Padding(
         padding: const EdgeInsets.all(16),
         child: Row(
@@ -93,5 +95,31 @@ class _HouseWorkItemState extends State<HouseWorkItem> {
     });
 
     widget.onCompleteTap(widget.houseWork);
+  }
+
+  Future<void> _onLongPress() async {
+    final action = await showModalBottomSheet<_ModalAction>(
+      context: context,
+      builder: (BuildContext context) {
+        return SafeArea(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ListTile(
+                leading: const Icon(Icons.delete),
+                title: const Text('削除する'),
+                onTap: () => Navigator.of(context).pop(_ModalAction.delete),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+
+    if (action != _ModalAction.delete) {
+      return;
+    }
+
+    widget.onDelete(widget.houseWork);
   }
 }
