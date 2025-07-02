1eb9ad30119ad9a651c0f5ab0f4e70db3c279850
shotaIDE <ide.shota.dev@gmail.com>
2025-07-02 12:14:44 +0900

    Update debug_screen.dart

--- client/lib/ui/feature/settings/debug_screen.dart
+++ client/lib/ui/feature/settings/debug_screen.dart
@@ -27,13 +27,13 @@ class DebugScreen extends ConsumerWidget {
           const _ToggleIsProTile(),
           const SectionHeader(title: 'チュートリアル'),
           _ResetReviewStatusTile(
-            keyDisplayName: '最初の家事のチュートリアル',
+            keyDisplayName: '家事ログ登録のチュートリアル',
             onReset: () => ref.read(
               resetHowToRegisterWorkLogsTutorialStatusProvider.future,
             ),
           ),
           _ResetReviewStatusTile(
-            keyDisplayName: '最初の家事ログのチュートリアル',
+            keyDisplayName: '家事ログと分析の確認のチュートリアル',
             onReset: () => ref.read(
               resetHowToCheckWorkLogsAndAnalysisTutorialStatusProvider.future,
             ),
