222b33653d3eba88b5e85f9a0a0e6080acc45d4c
Shota Ide <ide.shota.dev@gmail.com>
2025-07-03 20:42:50 +0900

    Update google_form_service.dart

--- client/lib/data/service/google_form_service.dart
+++ client/lib/data/service/google_form_service.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:logging/logging.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'google_form_service.g.dart';
@@ -25,6 +26,8 @@ class GoogleFormService {
     String? email,
     String? userId,
   }) async {
+    final logger = Logger('GoogleFormService');
+
     final formData = {
       'entry.893089758': feedback,
       if (email != null && email.isNotEmpty) 'entry.1495718762': email,
@@ -34,12 +37,18 @@ class GoogleFormService {
     const url =
         'https://docs.google.com/forms/d/1FAIpQLScS1p82L5tI4frPZLggUH35sbumRxK0EHvAEScNgck1Zv7gNg/formResponse';
 
-    await _dio.post<void>(
+    logger.info('Sending feedback to Google Form');
+
+    final response = await _dio.post<void>(
       url,
       data: formData,
       options: Options(
         contentType: 'application/x-www-form-urlencoded',
       ),
+    );
+
+    logger.info(
+      'Google Form response: status=${response.statusCode}, statusMessage=${response.statusMessage}',
     );
   }
 }
