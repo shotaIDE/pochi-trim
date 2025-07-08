538cd8bdf3578e79af29d3401d5ff3c9fc4fc35b
Shota Ide <ide.shota.dev@gmail.com>
2025-07-08 12:00:51 +0900

    Revert "fix"
    This reverts commit 2bbe28e04a395123fa70823ad7b478b5e3df2576.

--- client/android/app/build.gradle.kts
+++ client/android/app/build.gradle.kts
@@ -21,7 +21,7 @@ if (keystorePropertiesFile.exists()) {
 android {
     namespace = "ide.shota.colomney.pochi_trim"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
