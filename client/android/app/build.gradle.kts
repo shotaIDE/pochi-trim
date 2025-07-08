2bbe28e04a395123fa70823ad7b478b5e3df2576
Shota Ide <ide.shota.dev@gmail.com>
2025-07-08 12:00:47 +0900

    fix

--- client/android/app/build.gradle.kts
+++ client/android/app/build.gradle.kts
@@ -21,7 +21,7 @@ if (keystorePropertiesFile.exists()) {
 android {
     namespace = "ide.shota.colomney.pochi_trim"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
