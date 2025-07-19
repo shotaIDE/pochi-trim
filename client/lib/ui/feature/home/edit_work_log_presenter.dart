a74e360ca1cc2b660c05a75dc66544c523c26c7d
Shota Ide <ide.shota.dev@gmail.com>
2025-07-19 22:44:17 +0900

    Update edit_work_log_presenter.dart

--- client/lib/ui/feature/home/edit_work_log_presenter.dart
+++ client/lib/ui/feature/home/edit_work_log_presenter.dart
@@ -1,7 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:pochi_trim/data/model/update_work_log_exception.dart';
 import 'package:pochi_trim/data/repository/work_log_repository.dart';
-import 'package:pochi_trim/data/service/error_report_service.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'edit_work_log_presenter.g.dart';
@@ -13,12 +11,6 @@ Future<void> updateWorkLogDateTime(
   DateTime newDateTime,
 ) async {
   final workLogRepository = ref.read(workLogRepositoryProvider);
-  final errorReportService = ref.read(errorReportServiceProvider);
 
-  try {
-    await workLogRepository.updateDateTime(workLogId, newDateTime);
-  } on Exception catch (e, stack) {
-    await errorReportService.recordError(e, stack);
-    throw const UpdateWorkLogException();
-  }
+  await workLogRepository.updateDateTime(workLogId, newDateTime);
 }
