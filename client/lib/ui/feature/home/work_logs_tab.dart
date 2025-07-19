8b7354f841474d80c3fd35d9d1ad3ca72c722f5e
Shota Ide <ide.shota.dev@gmail.com>
2025-07-19 23:15:06 +0900

    refactor

--- client/lib/ui/feature/home/work_logs_tab.dart
+++ client/lib/ui/feature/home/work_logs_tab.dart
@@ -6,7 +6,7 @@ import 'package:pochi_trim/data/model/house_work.dart';
 import 'package:pochi_trim/data/model/work_log.dart';
 import 'package:pochi_trim/data/repository/dao/add_work_log_args.dart';
 import 'package:pochi_trim/data/repository/work_log_repository.dart';
-import 'package:pochi_trim/ui/feature/home/edit_work_log_screen.dart';
+import 'package:pochi_trim/ui/feature/home/edit_work_log_dialog.dart';
 import 'package:pochi_trim/ui/feature/home/work_log_included_house_work.dart';
 import 'package:pochi_trim/ui/feature/home/work_log_item.dart';
 import 'package:pochi_trim/ui/feature/home/work_logs_presenter.dart';
